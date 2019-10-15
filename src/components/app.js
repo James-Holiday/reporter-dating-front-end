fb4d6abe007ccbaea2a6c2196b85a059490aa14c
hdensten <hannah.densten@gmail.com>
2019-10-15 14:56:26 -0600

    First commit

--- src/components/app.js
+++ src/components/app.js
@@ -1,12 +1,14 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 export default class App extends Component {
   render() {
     return (
-      <div className='app'>
+      <div className="app">
         <h1>DevCamp React Starter</h1>
         <h2>React Redux Router</h2>
       </div>
     );
   }
 }
+
+// gh;sfjg;d
